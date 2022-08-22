a55a88d44196d541ec50300b13c6a11d3b443c8d
Denis <kade01@yandex.ru>
2022-08-22 15:51:16 +0300

    Задача номер 10

---
+++ ZADACHA_10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+System.Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(System.Console.ReadLine()!);
+System.Console.WriteLine($"Ваше второе число {number /10 %10}");
